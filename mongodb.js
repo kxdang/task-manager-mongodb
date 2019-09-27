bd097eebe5e7ccd3bb4ba3b53e273fae7358e6c0
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-27 18:25:25 -0400

    changing name to billy using the updateOne method and accessing via id and using set function

--- mongodb.js
+++ mongodb.js
@@ -21,39 +21,22 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").findOne(
-    //   { _id: new ObjectID("5d8cd7aa4db47847f41bc444") },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log("Unable to fetch");
-    //     }
-
-    //     console.log(user);
-    //   }
-    // );
-    // db.collection("users")
-    //   .find({ age: 25 })
-    //   .toArray((error, users) => {
-    //     console.log(users);
-    //   });
-
-    // db.collection("users")
-    //   .find({ age: 25 })
-    //   .count((error, count) => {
-    //     console.log(count);
-    //   });
-
-    db.collection("tasks").findOne(
-      { _id: new ObjectID("5d8cee00460c3447c851017d") },
-      (error, task) => {
-        console.log(task);
-      }
-    );
-
-    db.collection("tasks")
-      .find({ completed: false })
-      .toArray((error, notCompleted) => {
-        console.log(notCompleted);
+    db.collection("users")
+      .updateOne(
+        {
+          _id: new ObjectID("5d8cd94269884c1444533919")
+        },
+        {
+          $set: {
+            name: "Billy"
+          }
+        }
+      )
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 );
