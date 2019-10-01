691ae17908f528420ea9610fabfeef0d0b01fd59
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-01 19:03:45 -0400

    completing challenge requirements for seciton 86

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -33,11 +33,14 @@ const tasks = mongoose.model("tasks", {
 const User = mongoose.model("User", {
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error("Email is invalid");
@@ -46,17 +49,33 @@ const User = mongoose.model("User", {
   },
   age: {
     type: Number,
+    default: 0,
     validate(value) {
       if (value < 0) {
         throw new Error("age must be a positive number");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (validator.isLength(value, { min: 6, max: undefined })) {
+        if (value.toLowerCase().includes("password", 0)) {
+          throw new Error("Cannot contain password as pw");
+        }
+      } else {
+        throw new Error("Not long enough, must be 6 characters");
       }
     }
   }
 });
 
 const me = new User({
-  name: "Kien",
-  email: "mike@"
+  name: "Billy",
+  email: "myemail@gmail.com",
+  password: "Honey Bees Rock"
 });
 
 me.save()
