63aa48073c0676c0b206c0f26e58c3ef7d123ca8
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-01 16:04:56 -0400

    data validation using sanitization, using the validator npm library

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,22 +1,62 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-const User = mongoose.model("User", {
-  name: {
+const tasks = mongoose.model("tasks", {
+  description: {
     type: String
   },
+
+  completed: {
+    type: Boolean
+  }
+});
+
+// const todayTask = new tasks({
+//   description: "Complete Section 11 this week",
+//   completed: false
+// });
+
+// todayTask
+//   .save()
+//   .then(() => {
+//     console.log(todayTask);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
   age: {
-    type: Number
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("age must be a positive number");
+      }
+    }
   }
 });
 
 const me = new User({
-  name: "Kien Dang",
-  age: 69
+  name: "Kien",
+  email: "mike@"
 });
 
 me.save()
