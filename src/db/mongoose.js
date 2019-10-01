a0e6811f0f8e6c508772e08474baf5cde56a0c09
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-01 19:17:45 -0400

    completed challenge on 86

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,80 +8,83 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const tasks = mongoose.model("tasks", {
   description: {
-    type: String
-  },
-
-  completed: {
-    type: Boolean
-  }
-});
-
-// const todayTask = new tasks({
-//   description: "Complete Section 11 this week",
-//   completed: false
-// });
-
-// todayTask
-//   .save()
-//   .then(() => {
-//     console.log(todayTask);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
-const User = mongoose.model("User", {
-  name: {
     type: String,
     required: true,
     trim: true
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("age must be a positive number");
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (validator.isLength(value, { min: 6, max: undefined })) {
-        if (value.toLowerCase().includes("password", 0)) {
-          throw new Error("Cannot contain password as pw");
-        }
-      } else {
-        throw new Error("Not long enough, must be 6 characters");
-      }
-    }
+
+  completed: {
+    type: Boolean,
+    default: false
   }
 });
 
-const me = new User({
-  name: "Billy",
-  email: "myemail@gmail.com",
-  password: "Honey Bees Rock"
+const todayTask = new tasks({
+  description: "Dance at the Space   ",
+  completed: true
 });
 
-me.save()
+todayTask
+  .save()
   .then(() => {
-    console.log(me);
+    console.log(todayTask);
   })
   .catch(error => {
-    console.log("Error", error);
+    console.log(error);
   });
+
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Email is invalid");
+//       }
+//     }
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("age must be a positive number");
+//       }
+//     }
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     validate(value) {
+//       if (validator.isLength(value, { min: 6, max: undefined })) {
+//         if (value.toLowerCase().includes("password", 0)) {
+//           throw new Error("Cannot contain password as pw");
+//         }
+//       } else {
+//         throw new Error("Not long enough, must be 6 characters");
+//       }
+//     }
+//   }
+// });
+
+// const me = new User({
+//   name: "Billy",
+//   email: "myemail@gmail.com",
+//   password: "Honey Bees Rock"
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log("Error", error);
+//   });
