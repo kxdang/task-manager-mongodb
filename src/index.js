ba022944f73728f6facf1ebe88d8be0cc53adae0
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-09 10:24:39 -0400

    using password hasing library

--- src/index.js
+++ src/index.js
@@ -1,140 +1,34 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const task = require("./models/task");
+
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-//get all users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-//get a single user by id
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) {
-      return res.status(404).send;
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-//setting route to update a specific property via ID
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  //prettier-ignore
-  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates!" });
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["description", "completed"];
-  //prettier-ignore
-  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-
-  if (!isValidOperation) {
-    return res
-      .status(400)
-      .send({ error: "Invalid updates because no key is found!" });
-  }
-
-  try {
-    const singleTask = await task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!singleTask) {
-      return res.status(404).send();
-    }
-
-    res.send(singleTask);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-app.post("/tasks", async (req, res) => {
-  const newTask = new task(req.body);
-
-  try {
-    await newTask.save();
-    res.status(201).send(newTask);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-//get all tasks
-app.get("/tasks", async (req, res) => {
-  try {
-    const allTasks = await task.find({});
-    res.send(allTasks);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-//get a single task by ID
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const singleTask = await task.findById({ _id });
-    if (!singleTask) {
-      return res.status(404).send();
-    }
-    res.send(singleTask);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+const bcrypt = require("bcryptjs");
+
+const myFunction = async () => {
+  const password = "Red12345!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+  // bcrypt returns a promise, second param is number of cycles recommended by dev
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("Red12345!", hashedPassword);
+
+  console.log(isMatch);
+  // returns a boolean, library allows us to see if the password will match one in db
+};
+
+myFunction();
---
+++ src/routers/task.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = new express.Router();
+const task = require("../models/task");
+
+//Update a single task
+router.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  //prettier-ignore
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res
+      .status(400)
+      .send({ error: "Invalid updates because no key is found!" });
+  }
+
+  try {
+    const singleTask = await task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!singleTask) {
+      return res.status(404).send();
+    }
+
+    res.send(singleTask);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//updating tasks
+router.post("/tasks", async (req, res) => {
+  const newTask = new task(req.body);
+
+  try {
+    await newTask.save();
+    res.status(201).send(newTask);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//get all tasks
+router.get("/tasks", async (req, res) => {
+  try {
+    const allTasks = await task.find({});
+    res.send(allTasks);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+//get a single task by ID
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const singleTask = await task.findById({ _id });
+    if (!singleTask) {
+      return res.status(404).send();
+    }
+    res.send(singleTask);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+//delete task
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const singleTask = await task.findByIdAndDelete(req.params.id);
+
+    if (!singleTask) {
+      return res.status(404).send();
+    }
+
+    res.send(singleTask);
+  } catch (e) {
+    res.status(500);
+  }
+});
+
+module.exports = router;
