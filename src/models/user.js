597b9caae92bc7cb289222a3dd1f1c5d6fa0a704
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-11 17:30:57 -0400

    adding token at sign up

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -42,9 +43,27 @@ const userSchema = new mongoose.Schema({
         throw new Error("Not long enough, must be 6 characters");
       }
     }
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+//adding new method to login
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
 
+//Create new user
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -20,7 +22,8 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
