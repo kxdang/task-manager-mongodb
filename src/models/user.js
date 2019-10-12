a8201501f9391e3aecbff65609f3fb890e02ff39
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-12 17:35:47 -0400

    manging deletion of user

--- src/models/user.js
+++ src/models/user.js
@@ -110,7 +110,7 @@ userSchema.pre("save", async function(next) {
 
 // Delete user tasks when user is removed
 
-userSchema.pre("remove", async function() {
+userSchema.pre("remove", async function(next) {
   const user = this;
   await Task.deleteMany({ author: user._id });
 
