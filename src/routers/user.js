cdc39bc6dfa31a1ce8bd872f24b9ad8db0b2f281
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-23 15:44:31 -0400

    filter upload challenge

--- src/routers/user.js
+++ src/routers/user.js
@@ -91,7 +91,16 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: "avatar"
+  dest: "avatars",
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)/)) {
+      return cb(new Error("Please upload only jpg, jpeg or png"));
+    }
+    cb(undefined, true);
+  }
 });
 
 router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
