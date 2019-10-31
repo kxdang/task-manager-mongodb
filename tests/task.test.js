c207185c9cc53861cced9f64b1b25f2174ca5f73
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-31 17:47:34 -0400

    adding task testing after refactor

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,8 +1,20 @@
 const request = require("supertest");
+const app = require("../src/app");
 const Task = require("../src/models/task");
-const User = require("../src/models/user");
 const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
 
 beforeEach(setupDatabase);
 
-test("Should create task for user", () => {});
+test("Should create task for user", async () => {
+  const response = await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      description: "A task from my test"
+    })
+    .expect(201);
+
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toBe(false);
+});
