ef1f484f96b720c9c2d3802a5c316c5c74cb8af3
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-31 16:03:19 -0400

    refactoring to allow database data to be used within multiple test files

---
+++ tests/task.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const Task = require("../src/models/task");
+const User = require("../src/models/user");
+const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
+
+beforeEach(setupDatabase);
+
+test("Should create task for user", () => {});
