5a80f5d2f147dd225a0e76715d5ccbb7e548be45
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-10-31 11:00:09 -0400

    testing using authentication with jest and supertest

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,21 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = new mongoose.Types.ObjectId();
+
 const userOne = {
+  _id: userOneId,
   name: "Mike",
   email: "mike@example.com",
-  password: "56what!!"
+  password: "56what!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }
+  ]
 };
 
 beforeEach(async () => {
@@ -42,4 +52,16 @@ test("Should not login nonexisting user", async () => {
       password: "abc123456"
     })
     .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+  await request();
 });
